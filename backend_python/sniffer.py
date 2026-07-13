d591bb526150bc24623217abe9e79785404bb8fd
Wahiba Labrini <wahibalabrini@gmail.com>
2026-07-13 23:33:18 +0100

    Add sniffer.py: packet capture with IP extraction and database storage

--- backend_python/sniffer.py
+++ backend_python/sniffer.py
@@ -1,4 +1,13 @@
+from scapy.all import sniff,IP
 from db_handler import insert_alert
-print("le sniifer est en cours d'execution")
-insert_alert("192.150.2.1","192.160.100","brute","high")
+def process_packet( packet):
+    #print(packet.summary())print resume ready and now we are do that manuel
+    if packet.haslayer(IP) :
+        src_ip=packet[IP].src
+        dst_ip=packet[IP].dst
+        print(f"SOURCE: {src_ip}--> DESTINATION: {dst_ip}")
+        insert_alert(src_ip,dst_ip,"TEST CAPTURE","LOW")
+sniff(prn=process_packet,count=10)
+
+
         
