96215a937b03e0cccdc7b30a2db6b47f804caf74
Wahiba Labrini <wahibalabrini@gmail.com>
2026-07-13 23:24:47 +0100

    Refactor:Clean up console output messages in db_handler.py

--- backend_python/db_handler.py
+++ backend_python/db_handler.py
@@ -1,5 +1,6 @@
 import os
 import mysql.connector
+
 def insert_alert(source_ip,destination_ip,attack_type,risk_level):
     try :
         db_connection = mysql.connector.connect(
@@ -8,20 +9,20 @@ def insert_alert(source_ip,destination_ip,attack_type,risk_level):
             password=os.getenv('DB_PASSWORD'),
             database="nids_db"
         )
-        print('connection reusssite a la base donnee !')
+        print('CONNECTION REUSSIE A LA BASE DE DONNEES !')
        
-
         cursor=db_connection.cursor()
+
         sql_query="insert into alerts(source_ip,destination_ip,attack_type,risk_level) values(%s, %s, %s,%s)"
         data=(source_ip,destination_ip,attack_type,risk_level)
         cursor.execute(sql_query,data)
         db_connection.commit()
-        print(f"💾donnees inseree avec succes dans tableau 'alerts'!")
-    except mysql.connector.Error as error:
-        print(f"❌ ERREUR :{error}")
+        print(f"DONNEES INSEREES AVEC SUCCES DANS LE TABLEAU 'alerts'!")
+    except mysql.connector.Error as er:
+        print(f"ERREUR :{er}")
     finally:
         if'db_connection' in locals() and db_connection.is_connected():
             cursor.close()
             db_connection.close()
-            print("connection fermee")
+            print("CONNEXION FERMEE !")
         
